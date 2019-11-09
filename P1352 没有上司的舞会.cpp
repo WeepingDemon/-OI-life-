1a6b8c1221f8e8bcba2e09561868a9bc449537e5
WeepingDemon <48550237+WeepingDemon@users.noreply.github.com>
2019-11-09 08:45:10 +0800

    Create P1352 没有上司的舞会.cpp

---
+++ P1352 没有上司的舞会.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<cstring>
+#include<cstdio>
+#include<algorithm>
+using std::cin;using std::cout;using std::endl;using std::max;
+typedef long long ll;
+typedef unsigned long long ull;
+int n;
+bool havefather[6010];
+struct node{
+	int happy_point;
+	int sons[6010];	
+	int cnt;
+}a[6010];
+ll dp[6010][2];//dp[x][1]参加//dp[x][0]不参加 
+
+
+//dp[i][1] = max{dp[sonofi][0]
+void DP(int root){//flag表示其上司是否去
+	ll tmp = 0;
+	
+	dp[root][0] = 0;
+	dp[root][1] = a[root].happy_point;
+	
+	for(int i = 1;i <= a[root].cnt;i++){
+		int sn = a[root].sons[i];
+		DP(a[root].sons[i]);
+		dp[root][1] += dp[sn][0];
+		dp[root][0] += max(dp[sn][1],dp[sn][0]);
+	}
+}
+
+int main(){
+	cin >> n;
+	for(int i = 1;i <= n;i++){
+		cin >> a[i].happy_point;
+		a[i].cnt = 0;
+	}
+	for(int i = 1;i < n;i++){
+		int x,y;//xson,yfather
+		cin >> x >> y;
+		a[y].sons[++a[y].cnt] = x;
+		havefather[x] = true;
+	}
+	int root;
+	for(int i = 1;i <= n;i++){
+		if(!havefather[i]){
+			root = i;
+			break;
+		}
+	}
+	DP(root);
+	cout << max(dp[root][1],dp[root][0]);
+	return 0;
+}
