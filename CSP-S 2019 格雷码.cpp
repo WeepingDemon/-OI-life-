603f81148ef4de492610657fbb8af8ba0857a551
WeepingDemon <48550237+WeepingDemon@users.noreply.github.com>
2020-01-12 16:07:28 +0800

    位运算 结论

--- CSP-S 2019 格雷码.cpp
+++ CSP-S 2019 格雷码.cpp
@@ -8,7 +8,7 @@ ll k,base;
 int n,ans[180];
 
 int main(){
-	std::ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    std::ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     cin >> n >> k;
     k ^= (k >> 1);
     for(int i = n - 1;i >= 0;i--){
