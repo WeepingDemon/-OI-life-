636caf533eb99de47c269e3e96d7c6e42ab9072c
WeepingDemon <48550237+WeepingDemon@users.noreply.github.com>
2020-01-16 12:47:07 +0800

    并查集

--- P3367 【模板】并查集.cpp
+++ P3367 【模板】并查集.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int N,M,a[10001],op,x,y;
 
-int getfa(int step){
+inline int getfa(int step){
 	if(a[step] == step) return step;
 	else a[step] = getfa(a[step]);
 	return a[step];
@@ -10,13 +10,13 @@ int getfa(int step){
 
 int main(){
 	cin >> N >> M;
-	for(int i = 1;i <= N;i++) a[i] = i;
-	for(int i = 1;i <= M;i++){
+	for(register int i = 1;i <= N;++i) a[i] = i;
+	for(register int i = 1;i <= M;++i){
 		cin >> op >> x >> y;
 		if(op == 1) a[getfa(x)] = a[getfa(y)];
 		else if(op == 2)
-		 if(a[getfa(x)] == a[getfa(y)]) cout << "Y" << endl;
-		 else cout << "N" << endl;
+		 if(a[getfa(x)] == a[getfa(y)]) cout << "Y\n";
+		 else cout << "N\n";
 	}
 	return 0;
 }
