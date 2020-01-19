e5328f4398ec0f9b015f06b3f5975ed89457e614
WeepingDemon <48550237+WeepingDemon@users.noreply.github.com>
2020-01-19 21:12:32 +0800

    字符串匹配 KMP

--- P3375 【模板】KMP字符串匹配.cpp
+++ P3375 【模板】KMP字符串匹配.cpp
@@ -5,24 +5,24 @@ char s1[10001],s2[10001];
 int i,j,kmp[10001],len1,len2;
 
 int main(){
-    std::cin>>s1+1>>s2+1;
-    len1=strlen(s1+1);
-    len2=strlen(s2+1);
+    std::cin >> s1 + 1 >> s2 + 1;
+    len1 = strlen(s1 + 1);
+    len2 = strlen(s2 + 1);
     j=0;
-    for(i=2;i<=len2;i++){
-        while(j&&s2[i]!=s2[j+1]) j=kmp[j];
-        if(s2[i]==s2[j+1]){
-            kmp[i]=++j;
+    for(i = 2;i <= len2;++i){
+        while(j && s2[i] != s2[j + 1]) j = kmp[j];
+        if(s2[i] == s2[j + 1]){
+            kmp[i] = ++j;
         }
     }
-    j=0;
-    for(i=1;i<=len1;i++){
-        while(j&&s1[i]!=s2[j+1]) j=kmp[j];
-        if(s2[j+1]==s1[i]) j++;
+    j = 0;
+    for(i = 1;i <= len1;++i){
+        while(j && s1[i] != s2[j + 1]) j = kmp[j];
+        if(s2[j + 1] == s1[i]) ++j;
         //else j=0;
-        if(j==len2) std::cout<<i-len2+1<<std::endl;
+        if(j == len2) std::cout << i - len2 + 1 << "\n";
     }
-    for(int i=1;i<=len2;i++){
-        std::cout<<kmp[i]<<" ";
+    for(int i = 1;i <= len2;++i){
+        std::cout << kmp[i] << " ";
     }
 }
